8f439824218e6c4f0cb5d178d58a264e2d4801a6
toukashishigaya <ken12345.097@gmail.com>
2023-02-05 20:53:07 +0700

    bf419

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b 4.4-13 lm
+git clone git@github.com:Mocaness/local_manifests.git -b 4.19-13 lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
@@ -12,7 +12,7 @@ source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch derp_whyred-userdebug
+lunch derp_whyred-user
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
