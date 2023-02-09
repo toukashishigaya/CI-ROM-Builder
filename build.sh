8918c431f2a96d32ede35115d90db6a333a00374
toukashishigaya <ken12345.097@gmail.com>
2023-02-09 23:14:27 +0700

    qwi

--- build.sh
+++ build.sh
@@ -1,23 +1,22 @@
 # sync rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b 4.4-13 lm
+git clone git@github.com:Mocaness/local_manifests.git -b qassa-whyred lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-bash $CIRRUS_WORKING_DIR/script/pickme.sh
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch derp_whyred-userdebug
+lunch qassa_whyred-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka derp -j16  > reading & sleep 95m
+mka qassa -j16  > reading & sleep 95m
 
 retVal=$?
 timeEnd
