4842919170781f510a964eb161314e3d2bd1aaa3
toukashishigaya <ken12345.097@gmail.com>
2023-02-11 08:49:47 +0700

    uffps

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,5 +1,5 @@
 cd packages/apps/DerpLauncher
-git fetch https://github.com/DF-Whyred/packages_apps_DerpLauncher 4337765405864b8e754b6f85a7bf4ccd26f75532
+git fetch https://github.com/DF-Whyred/packages_apps_DerpLauncher 2599b2380ab23d80d0f36c5db65f30223f2732f0
 git cherry-pick FETCH_HEAD
 cd ../
 cd Settings
