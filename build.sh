8e4ee8f7ecac36257ce9c91b03201536722b7003
toukashishigaya <ken12345.097@gmail.com>
2023-02-13 19:48:32 +0700

    blfw

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 ssh://git@github.com/Mocaness/device_xiaomi_mido -b qassa-4.9 device/xiaomi/mido
-git clone --depth=1 ssh://git@github.com/Mocaness/device_xiaomi_mido-kernel -b 10 device/xiaomi/mido-kernel
-git clone --depth=1 ssh://git@github.com/Mocaness/MidoKernel -b kizuna-17.1-C17 kernel/xiaomi/mido
-git clone --depth=1 ssh://git@github.com/Mocaness/vendor_xiaomi_mido -b qassa-4.9 vendor/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_device_xiaomi_whyred -b lineage-17.1 device/xiaomi/whyred
+git clone --depth=1 https://github.com/LineageOS/android_device_xiaomi_sdm660-common -b lineage-17.1 device/xiaomi/sdm660-common
+git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_sdm660 -b lineage-17.1 kernel/xiaomi/sdm660
+git clone --depth=1 https://gitlab.com/the-muppets/proprietary_vendor_xiaomi -b lineage-17.1 vendor/xiaomi
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch mido-userdebug
+lunch whyred-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka qassa -j16  > reading & sleep 95m
+mka bacon -j16  > reading & sleep 95m
 
 retVal=$?
 timeEnd
