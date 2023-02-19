693fa622d0c040ee76faa054533ecdc63503ac51
toukashishigaya <ken12345.097@gmail.com>
2023-02-19 12:32:45 +0700

    sbf318

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b qassa-mido lm
+git clone git@github.com:Mocaness/local_manifests.git -b qassa-mido-318 lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
