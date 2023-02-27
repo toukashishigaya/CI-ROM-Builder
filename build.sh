b9678e62e479338595abbf875c6f35710fa95f70
toukashishigaya <ken12345.097@gmail.com>
2023-02-28 00:30:55 +0700

    icfw

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/CarbonROM/android.git -b cr-11.0 -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b qassa-whyred lm
+git clone git@github.com:Mocaness/local_manifests.git -b cr-a11-whyred lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
@@ -11,12 +11,12 @@ source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch qassa_whyred-userdebug
+lunch cr_whyred-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka qassa -j16  > reading & sleep 95m
+mka carbon -j16  > reading & sleep 95m
 
 retVal=$?
 timeEnd
