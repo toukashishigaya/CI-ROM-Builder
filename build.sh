7ad9f025bb4a70f5acf20a91cefb889daa561b04
toukashishigaya <ken12345.097@gmail.com>
2023-03-08 18:48:40 +0700

    ifa12

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b 4.4-13 lm
+git clone git@github.com:Mocaness/local_manifests.git -b 4.4-12 lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
