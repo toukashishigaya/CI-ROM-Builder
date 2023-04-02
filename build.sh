232106e37b2056e84b40e1adcaf4c4435f52a119
toukashishigaya <ken12345.097@gmail.com>
2023-04-02 23:57:32 +0700

    init new script

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
 rm -rf lm
+git clone git@github.com:Mocaness/local_manifests.git -b qassa-whyred lm
 rm -rf .repo/local_manifests
-git clone git@github.com:Mocaness/local_manifests.git -b 4.4-12 lm
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-bash $CIRRUS_WORKING_DIR/script/pickme.sh
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 15G
-./barom.sh --device whyred --lunch derp_whyred-userdebug
-./barom.sh -b -j 8 -u wet --timer 82m -- m derp
+./barom.sh --device whyred --lunch qassa_whyred-userdebug
+./barom.sh -b -j 8 -u wet --timer 82m -- m qassa
 
 # end
--- script/pickme.sh
+++
@@ -1,1 +0,0 @@
-curl https://github.com/DerpFest-12/system_core/commit/1eb28844c7a9979f449864fce7da51f5dce691e7.patch | git -C "system/core" am
