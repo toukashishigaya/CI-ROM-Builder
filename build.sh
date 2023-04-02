51b0ebdb3874f24f8c7307c786d9ff2f4e1b70a9
toukashishigaya <ken12345.097@gmail.com>
2023-04-03 00:08:13 +0700

    build mmeedo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u ssh://git@github.com/keepQASSA/manifest -b Q -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b qassa-whyred lm
+git clone git@github.com:Mocaness/local_manifests.git -b qassa-mido-318 lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
@@ -11,7 +11,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/toukashishigaya/barom/main/b
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 15G
-./barom.sh --device whyred --lunch qassa_whyred-userdebug
+./barom.sh --device whyred --lunch qassa_mido-userdebug
 ./barom.sh -b -j 8 -u gof --timer 82m -- m qassa
 
 # end
