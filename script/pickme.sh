1280bc0a87f7beacf0023db826da371717bd8bc2
toukashishigaya <ken12345.097@gmail.com>
2023-04-03 13:29:26 +0700

    run a10 metalava hax

---
+++ script/pickme.sh
@@ -0,0 +1,1 @@
+curl https://github.com/Magma-WIP/build_soong/commit/bcd1bb529132905cf55e72f5a2a6ba19a99f60ac.patch | git -C "build/soong" am && curl https://github.com/Magma-WIP/build_soong/commit/dc3365fbde3b2a5773e655f690bb073967100795.patch | git -C "build/soong" am
