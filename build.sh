b4094a1fe49e40de21483960c0700a7135c2d65c
toukashishigaya <ken12345.097@gmail.com>
2023-04-08 00:25:16 +0700

    ifcib419

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 rm -rf lm
-git clone git@github.com:Mocaness/local_manifests.git -b 4.4-13 lm
+git clone git@github.com:Mocaness/local_manifests.git -b 4.19-13 lm
 rm -rf .repo/local_manifests
 cp -r lm/local_manifests .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
@@ -12,7 +12,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 15G
-./barom.sh --device whyred --lunch derp_whyred-userdebug
+./barom.sh --device whyred --lunch derp_whyred-user
 ./barom.sh -b -j 8 -u gof --timer 95m -- m derp
 
 # end
