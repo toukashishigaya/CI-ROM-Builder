34cc1ff8290ef0672cc1e17ae6893923bc701c0a
toukashishigaya <ken12345.097@gmail.com>
2023-04-12 19:01:12 +0700

    also patch derpspace

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,1 +1,2 @@
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
+curl https://github.com/DF-Whyred/packages_apps_DerpSpace/commit/27085d8c3e93dce70f2f1387b4639001399ec167.patch | git -C "packages/apps/DerpSpace" am
