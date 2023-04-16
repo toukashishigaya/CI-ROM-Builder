46003f05be4dbdb51b3a58c49f892cdf46ae9147
toukashishigaya <ken12345.097@gmail.com>
2023-04-16 12:52:04 +0700

    also disable autobrightness on derpspace

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,3 +1,3 @@
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
-curl https://github.com/DF-Whyred/packages_apps_DerpSpace/commit/27085d8c3e93dce70f2f1387b4639001399ec167.patch | git -C "packages/apps/DerpSpace" am
+curl https://github.com/DF-Whyred/packages_apps_DerpSpace/commit/708f03f91ab4cb9ab77aad288693f126ebac1882.patch | git -C "packages/apps/DerpSpace" am
 curl https://github.com/DF-Whyred/vendor_derp/commit/ab8e0ea0d35e66543c2cd93638daa74d69971b0a.patch | git -C "vendor/derp" am
