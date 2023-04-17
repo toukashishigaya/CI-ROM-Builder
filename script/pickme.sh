a537ac11b874ea158cb7f632533836b96afd0d31
toukashishigaya <ken12345.097@gmail.com>
2023-04-17 21:15:45 +0700

    drop some pickme

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,5 +1,2 @@
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
-curl https://github.com/DF-Whyred/packages_apps_DerpSpace/commit/708f03f91ab4cb9ab77aad288693f126ebac1882.patch | git -C "packages/apps/DerpSpace" am
 curl https://github.com/DF-Whyred/vendor_derp/commit/ab8e0ea0d35e66543c2cd93638daa74d69971b0a.patch | git -C "vendor/derp" am
-curl https://github.com/DF-Whyred/packages_apps_DerpLauncher/commit/98734a3b3e9f8ac598af3069e8b5de04b3f9b37f.patch | git -C "packages/apps/DerpLauncher" am
-curl https://github.com/DF-Whyred/packages_apps_DerpLauncher/commit/b45005e0e60f3842bd344fec2fb150c6ba92616f.patch | git -C "packages/apps/DerpLauncher" am
