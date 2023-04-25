0ade9833421f160c1dd2b56bbdb1768485b867e7
toukashishigaya <ken12345.097@gmail.com>
2023-04-25 18:07:09 +0700

    anu pick for 12l temp

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,1 +1,5 @@
-curl https://github.com/DerpFest-12/system_core/commit/1eb28844c7a9979f449864fce7da51f5dce691e7.patch | git -C "system/core" am
+curl https://github.com/DF-Whyred/system_core/commit/67debe39d8c985e8d4bae4b83b18e8d74e3081a6.patch | git -C "system/core" am
+curl https://github.com/DF-Whyred/system_core/commit/73967771041a215880ea78fec5f2deefae378bf1.patch | git -C "system/core" am
+curl https://github.com/DF-Whyred/vendor_derp/commit/827d9c7690dd3cc08b9262470940afb9867c3e68.patch | git -C "vendor/derp" am
+curl https://github.com/DF-Whyred/vendor_derp/commit/4902846e552662ca51e97f3d586c835e17861d9e.patch | git -C "vendor/derp" am
+curl https://github.com/DF-Whyred/vendor_derp/commit/162d207b443a7966e431cd7fc1c7f29c576ebaa3.patch | git -C "vendor/derp" am
