3f30bc5e8d73cee359364cf10cc80c351b616045
toukashishigaya <ken12345.097@gmail.com>
2023-04-25 20:42:21 +0700

    switch to cherry-pick

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,6 +1,4 @@
 curl https://github.com/DF-Whyred/system_core/commit/67debe39d8c985e8d4bae4b83b18e8d74e3081a6.patch | git -C "system/core" am
 curl https://github.com/DF-Whyred/system_core/commit/73967771041a215880ea78fec5f2deefae378bf1.patch | git -C "system/core" am
-curl https://github.com/DF-Whyred/vendor_derp/commit/c36dcc9fe9fdf86a7b3c394db2cadd81f173c1ce.patch | git -C "vendor/derp" am
-curl https://github.com/DF-Whyred/vendor_derp/commit/827d9c7690dd3cc08b9262470940afb9867c3e68.patch | git -C "vendor/derp" am
-curl https://github.com/DF-Whyred/vendor_derp/commit/4902846e552662ca51e97f3d586c835e17861d9e.patch | git -C "vendor/derp" am
-curl https://github.com/DF-Whyred/vendor_derp/commit/162d207b443a7966e431cd7fc1c7f29c576ebaa3.patch | git -C "vendor/derp" am
+git -C "vendor/derp" fetch https://github.com/DF-Whyred/vendor_derp 12.1 --depth=5
+git -C "vendor/derp" cherry-pick 827d9c7690dd3cc08b9262470940afb9867c3e68^..162d207b443a7966e431cd7fc1c7f29c576ebaa3
