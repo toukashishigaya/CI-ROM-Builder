cde9931980fccf4254b6b20aaeaab24c07d606f1
toukashishigaya <ken12345.097@gmail.com>
2023-04-25 15:11:10 +0700

    increase to 92m

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@ chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 15G
 ./barom.sh --device whyred --lunch whyred-user
-./barom.sh -b -j 8 -u gof --timer 89m -- m dudu
+./barom.sh -b -j 8 -u gof --timer 92m -- m dudu
 
 # end
