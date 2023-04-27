1184f551e1ac96530090a48eea51888f9f27c33a
toukashishigaya <ken12345.097@gmail.com>
2023-04-27 09:07:41 +0700

    also pick futex vendor

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,1 +1,2 @@
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
+curl https://github.com/DF-Whyred/vendor_derp/commit/902164c0ec774880753bfae46a96cfbccb5ee669.patch | git -C "vendor/derp" am
