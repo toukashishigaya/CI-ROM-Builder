72c612ecafd4c9a7e7fd272354d6df7f797cceb7
toukashishigaya <ken12345.097@gmail.com>
2023-04-27 10:14:34 +0700

    also patch soong for malloc

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,2 +1,3 @@
+curl https://github.com/DF-Whyred/build_soong/commit/5b9de8a72010fbcca16f5bf21ad2dd04def699d5.patch | git -C "build/soong" am
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
 curl https://github.com/DF-Whyred/vendor_derp/commit/902164c0ec774880753bfae46a96cfbccb5ee669.patch | git -C "vendor/derp" am
