581a2c57888c77261dfe172bd902ab35b87ccc80
toukashishigaya <ken12345.097@gmail.com>
2023-04-28 15:43:28 +0700

    also pick fwb changes from mine

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,3 +1,6 @@
 curl https://github.com/DF-Whyred/build_soong/commit/5b9de8a72010fbcca16f5bf21ad2dd04def699d5.patch | git -C "build/soong" am
+curl https://github.com/DF-Whyred/frameworks_base/commit/c67a3bc15d670a913d43a87edf3a766695462dc0.patch | git -C "frameworks/base" am
+curl https://github.com/DF-Whyred/frameworks_base/commit/6d7e22a1fed9ec99be5d9f6e2389f3247dea3070.patch | git -C "frameworks/base" am
+curl https://github.com/DF-Whyred/frameworks_base/commit/fdcebacde0dbd1dabaa690de49b6e9dc4589da64.patch | git -C "frameworks/base" am
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
 curl https://github.com/DF-Whyred/vendor_derp/commit/902164c0ec774880753bfae46a96cfbccb5ee669.patch | git -C "vendor/derp" am
