cb7cb0e70d2574b85661efb8432e8bad369a8151
shitoruka <kasumeme.yukina@gmail.com>
2023-05-04 04:26:38 +0700

    Revert "pick lens chgs"
    This reverts commit a3f117138161aed453ecca5b6dcfc6dcc61df4de.

--- script/pickme.sh
+++ script/pickme.sh
@@ -1,5 +1,4 @@
 curl https://github.com/DF-Whyred/build_soong/commit/5b9de8a72010fbcca16f5bf21ad2dd04def699d5.patch | git -C "build/soong" am
 curl https://github.com/DF-Whyred/frameworks_base/commit/fdcebacde0dbd1dabaa690de49b6e9dc4589da64.patch | git -C "frameworks/base" am
-curl https://github.com/DF-Whyred/frameworks_base/commit/74993c12c9767e634e07b4061b39f219ab739ddb.patch | git -C "frameworks/base" am
 curl https://github.com/DF-Whyred/system_core/commit/959c08789ee50dc02fd4512376539ae74ea48955.patch | git -C "system/core" am
 curl https://github.com/DF-Whyred/vendor_derp/commit/902164c0ec774880753bfae46a96cfbccb5ee669.patch | git -C "vendor/derp" am
